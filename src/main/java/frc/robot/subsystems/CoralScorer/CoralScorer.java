38888ac227016d612341253e64eb44191688f0bf
OscarCDavis <oscarcdavis@gmail.com>
2025-01-25 16:59:28 -0700

    Coral score

---
+++ src/main/java/frc/robot/subsystems/CoralScorer/CoralScorer.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems.CoralScorer;
+
+import frc.robot.util.RBSISubsystem;
+
+public class CoralScorer extends RBSISubsystem {
+  private final CoralScorerIO io;
+
+
+  public CoralScorer(CoralScorerIO io) {
+    this.io = io;
+  }
+  
+
+  
+}
---
+++ src/main/java/frc/robot/subsystems/CoralScorer/CoralScorerIO.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems.CoralScorer;
+
+public interface CoralScorerIO {
+  
+}

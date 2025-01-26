ffcc1267348e351b2a4d69fdcc396fe191507756
OscarCDavis <oscarcdavis@gmail.com>
2025-01-25 18:35:24 -0700

    coral scorer stuff

--- src/main/java/frc/robot/subsystems/CoralScorer/CoralScorerIO.java
+++ src/main/java/frc/robot/subsystems/CoralScorer/CoralScorerIO.java
@@ -1,6 +1,11 @@
 package frc.robot.subsystems.CoralScorer;
 
+import org.littletonrobotics.junction.AutoLog;
+
 public interface CoralScorerIO {
+
+  @AutoLog
+  public static class IntakeIOInputs {}
 
   public default void setVolts(double volts) {}
 

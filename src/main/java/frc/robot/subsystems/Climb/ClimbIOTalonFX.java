28a319afc35f525abc8b3d451f56b163bcffd724
Connor Norris <99221420+ConnorJNorris@users.noreply.github.com>
2025-02-01 12:42:33 -0700

    Made climb commands

---
+++ src/main/java/frc/robot/subsystems/Climb/ClimbIOTalonFX.java
@@ -0,0 +1,42 @@
+package frc.robot.subsystems.Climb;
+
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.PositionDutyCycle;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj.Servo;
+
+public class ClimbIOTalonFX implements ClimbIO{
+  private final Servo climbExtender = new Servo(1);
+  private final TalonFX climber = new TalonFX(31);
+    public ClimbIOTalonFX() {
+      var climbConfig = new TalonFXConfiguration();
+      climbConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    }
+    @Override
+    public void turnClimbServo(double position) {
+      climbExtender.set(position);
+    }
+
+    @Override
+    public void twistMotorToPosition(double position) {
+      climber.setControl(new PositionDutyCycle(position));
+    }
+
+    @Override
+    public void twistMotorVolts(double volts) {
+      climber.setVoltage(volts);
+    }
+
+    @Override
+    public void configPID(double kP, double kI, double kD) {
+      Slot0Configs pid = new Slot0Configs();
+      pid.withKP(kP);
+      pid.withKI(kI);
+      pid.withKD(kD);
+      climber.getConfigurator().apply(pid);
+    }
+
+    }

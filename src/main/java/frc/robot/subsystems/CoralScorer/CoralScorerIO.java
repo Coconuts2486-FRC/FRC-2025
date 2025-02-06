8ac8aa94dfba5bf9bda57a1980309c102890e90d
OscarCDavis <oscarcdavis@gmail.com>
2025-02-05 17:02:37 -0700

    finished io class

--- src/main/java/frc/robot/subsystems/CoralScorer/CoralScorerIO.java
+++ src/main/java/frc/robot/subsystems/CoralScorer/CoralScorerIO.java
@@ -1,3 +1,17 @@
 package frc.robot.subsystems.CoralScorer;
 
-public class CoralScorerIO {}
+import org.littletonrobotics.junction.AutoLog;
+
+public interface CoralScorerIO {
+
+  @AutoLog
+  public static class IntakeIOInputs {}
+
+  public final int[] powerPorts = {};
+
+  public default void setVolts(double volts) {}
+
+  public default void stop() {}
+
+  public default void setVelocity(double velocity) {}
+}

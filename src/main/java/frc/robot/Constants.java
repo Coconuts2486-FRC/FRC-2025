f99560badb8ccd0d123fd7d01263db7a6288c61f
Timothy P. Ellsworth Bowers <tbowers@lowell.edu>
2025-03-20 17:27:19 -0700

    Move Drive2Pose constants to Constants
    	modified:   src/main/java/frc/robot/Constants.java
    	modified:   src/main/java/frc/robot/commands/DriveToPose.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -451,7 +451,7 @@ public final class Constants {
     public static final PIDConstants kPPdrivePID = new PIDConstants(10, 0, 0.2);
     // new PIDConstants(DrivebaseConstants.kDriveP, 0.0, DrivebaseConstants.kDriveD);
     public static final PIDConstants kPPsteerPID = new PIDConstants(4, 0.0, 0.0);
-    // new PIDConstants(DrivebaseConstants.kSteerP, 0.0, DrivebaseConstants.kSteerD);
+
     // 1 Cordinate = 1 meter
     // *** 1 meter = 39.3701 inches
     // Pathplanner Maxiums with MK4i L3 with an amp limit of 40 Amps and other robot specifics
@@ -484,6 +484,42 @@ public final class Constants {
     // Drive and Turn PID constants used for Chorep
     public static final PIDConstants kChoreoDrivePID = new PIDConstants(10.0, 0.0, 0.0);
     public static final PIDConstants kChoreoSteerPID = new PIDConstants(7.5, 0.0, 0.0);
+  }
+
+  public static class DriveToPositionConstatnts {
+
+    // The robot is facing AWAY from the tag, so its pose angle matches that of the tag.
+    // Scoring position has the bumpers 3" from the tag.  Bumper-to-center distance is 18", ergo the
+    // robot pose is 21" from the tag.
+    public static Translation2d kLeftReefPost =
+        new Translation2d(Units.inchesToMeters(18), Units.inchesToMeters(-6.75));
+    public static Translation2d kRightReefPost =
+        new Translation2d(Units.inchesToMeters(18), Units.inchesToMeters(+6.75));
+    public static Translation2d kRightReefPostClose =
+        new Translation2d(Units.inchesToMeters(16.75), Units.inchesToMeters(+6.75));
+    public static Translation2d kAlgaeGrab =
+        new Translation2d(Units.inchesToMeters(26.0), Units.inchesToMeters(0.0));
+
+    // Constants used by the DriveToPose Command
+    public static final double drivekP = AutoConstants.kPPdrivePID.kP; // 9.0;
+    public static final double drivekD = AutoConstants.kPPdrivePID.kD; // 0.0;
+    public static final double thetakP = AutoConstants.kPPsteerPID.kP; // 4.0;
+    public static final double thetakD = AutoConstants.kPPsteerPID.kD; // 0.0;
+
+    // Values in m/s
+    public static final double driveMaxVelocity = 3.0;
+    public static final double driveMaxVelocitySlow = 1.0;
+    public static final double driveMaxAcceleration = 2.5;
+    public static final double thetaMaxVelocity = Units.degreesToRadians(360.0);
+    public static final double thetaMaxVelocitySlow = Units.degreesToRadians(90.0);
+    public static final double thetaMaxAcceleration = Units.degreesToRadians(720.0);
+
+    public static final double driveTolerance = 0.01;
+    public static final double driveToleranceSlow = 0.06;
+    public static final double thetaTolerance = Units.degreesToRadians(1.0);
+    public static final double thetaToleranceSlow = Units.degreesToRadians(3.0);
+    public static final double ffMinRadius = 0.2;
+    public static final double ffMaxRadius = 0.8;
   }
 
   /** LED Constants ******************************************************** */
